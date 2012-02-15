f1e60d76ca36dd3245f465cb0689d7734280d82d
Julio Capote <jcapote@gmail.com>
2012-02-15 14:35:33 -0800

    fuck tests

--- src/main/scala/com/posterous/App.scala
+++ src/main/scala/com/posterous/App.scala
@@ -1,15 +1,28 @@
 package com.posterous
 
+import java.net.InetSocketAddress;
+
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.handler.codec.http.HttpVersion;
+
+import com.twitter.finagle.Service;
+import com.twitter.finagle.builder.ServerBuilder;
+import com.twitter.finagle.http.Http;
+import com.twitter.util.Future;
+import com.twitter.util.FutureEventListener;
+import com.twitter.util.FutureTransformer;
+
 /**
  * @author ${user.name}
  */
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
   def main(args : Array[String]) {
     println( "Hello World!" )
-    println("concat arguments = " + foo(args))
   }
 
 }
